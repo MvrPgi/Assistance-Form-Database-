ed63a3b6e18a49446281b11ef5a6f406e4ec875b
Mover <movermercene@gmail.com>
2024-05-14 10:07:17 +0800

    V1 UI

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="1234",
-  database="IMDB" # Connect to the database   STEP 3
+#  database="IMDB" # Connect to the database   STEP 3
 
 )
 #
