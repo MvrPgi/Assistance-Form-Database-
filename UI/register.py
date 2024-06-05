d42dc02d773562cb452fdfe619d7f5a16b2d4298
Mover <movermercene@gmail.com>
2024-06-05 23:20:30 +0800

    hide/show/button/

--- UI/register.py
+++ UI/register.py
@@ -29,17 +29,14 @@ class Register(tk.Canvas):
         super().__init__(master, height=500, width=820, bg="#FFFFFF", highlightthickness=0)
 
 
-
-        #CREATE THE TEXTS
         
+        #CREATE THE TEXTS        
         self.create_text(49.0, 100.0, anchor="nw", text="Reference No:", fill="#000000", font=("Mada Regular", 10 * -1))
         self.create_text(289.0, 100,anchor ="nw", text="Date:",fill="#000000",font=("Mada Regular", 10 * -1))
-        ############################
         self.create_text(49.0,172.0,anchor ="nw", text="Full Name Of The Patient    ",fill="#000000",font=("Mada Regular", 10 * -1))
         self.create_text(87.0,203.0,anchor ="nw", text="Surname",fill="#000000",font=("Mada Regular", 8 * -1))   
         self.create_text(385., 205,anchor ="nw", text="First Name", fill="#000000",font=("Mada Regular", 8 * -1))
         self.create_text(691.0,204,anchor ="nw", text="Middle Name",fill="#000000",font=("Mada Regular", 8 * -1))
-        ############################
         self.create_text(49.0, 219.0,anchor ="nw", text="Permanent Address",fill="#000000",font=("Mada Regular", 10 * -1))
         self.create_text(87.0, 250.0,anchor ="nw", text="No",fill="#000000",font=("Mada Regular", 8 * -1))
         self.create_text(120.0, 250.0,anchor ="nw", text="Street",fill="#000000",font=("Mada Regular", 8 * -1))
@@ -47,13 +44,10 @@ class Register(tk.Canvas):
         self.create_text(320.0, 250.0,anchor ="nw", text="Municipality/City",fill="#000000",font=("Mada Regular", 8 * -1))
         self.create_text(500.0, 250.0,anchor ="nw", text="Province",fill="#000000",font=("Mada Regular", 8 * -1))
         self.create_text(700, 250.0,anchor ="nw", text="Region",fill="#000000",font=("Mada Regular", 8 * -1))
-        ############################
         self.create_text(49.0, 265.0,anchor ="nw", text="Civil Status",fill="#000000",font=("Mada Regular", 10 * -1))
-        ############################
         self.create_text(49.0, 290.0,anchor ="nw", text="Birthdate:",fill="#000000",font=("Mada Regular", 10 * -1))
         self.create_text(227.0, 290.0,anchor ="nw", text="Age:",fill="#000000",font=("Mada Regular", 10 * -1))
-        self.create_text(420.0, 290.0,anchor ="nw", text = "Sex",fill="#000000",font=("Mada Regular", 10 * -1))
-        ############################           
+        self.create_text(420.0, 290.0,anchor ="nw", text = "Sex",fill="#000000",font=("Mada Regular", 10 * -1))         
         self.create_text(49.0, 321.5,anchor ="nw", text = "Nationality",fill="#000000",font=("Mada Regular", 10 * -1))
         self.create_text(227.0, 321.5,anchor ="nw", text = "Religion",fill="#000000",font=("Mada Regular", 10 * -1))
         self.create_text(49.0, 353, anchor ="nw", text = "Highest Educational Attainment",fill="#000000",font=("Mada Regular", 10 * -1))
@@ -64,6 +58,8 @@ class Register(tk.Canvas):
         self.create_text(420.0, 390, anchor ="nw", text = "Monthly Expenditure",fill="#000000",font=("Mada Regular", 10 * -1))
         self.create_text(559.0, 390, anchor ="nw", text = "Gross Monthly Income",fill="#000000",font=("Mada Regular", 10 * -1))
         self.create_text(700.0, 390, anchor ="nw", text = "Net Monthly Income",fill="#000000",font=("Mada Regular", 10 * -1))
+
+
         #LOAD THE IMAGES
         self.image_image_1 = PhotoImage(file=resource_path("resources/APP_1/image_1.png"))  
         self.image_entry_3 = PhotoImage(file=resource_path("resources/APP_1/entry_3.png"))
@@ -72,32 +68,78 @@ class Register(tk.Canvas):
         self.image_entry_11 = PhotoImage(file=resource_path("resources/APP_1/entry_11.png"))
         self.image_entry_12 = PhotoImage(file=resource_path("resources/APP_1/entry_12.png"))    
         self.image_entry_9 = PhotoImage(file=resource_path("resources/APP_1/entry_9.png"))
+        self.image_entry_16 = PhotoImage(file=resource_path("resources/APP_1/entry_16.png"))
+        self.button_image_1 = PhotoImage(file=resource_path("resources/APP_1/button_1.png"))
        
-
-        #PLACE THE IMAGES
-        self.create_image(405.0, 46.0, image=self.image_image_1)
-        self.create_image(120.0, 150.5, image=self.image_image_6)
-        self.create_image(409.0, 190.5, image=self.image_entry_3)
-        self.create_image(409.0, 237.5, image=self.image_entry_3)
-        self.create_image(397.0, 120.5, image=self.image_entry_11)
-        self.create_image(155.5, 120.5, image=self.image_entry_11)
-        self.create_image(120.5, 310.5, image=self.image_entry_5)
-        self.create_image(298.5, 310.5, image=self.image_entry_5)   
-        self.create_image(120.5, 340.5, image=self.image_entry_5)
-        self.create_image(298.5, 340.5, image=self.image_entry_5)
-        self.create_image(490.5, 342.5, image=self.image_entry_5)
-        self.create_image(698.5, 342.5, image=self.image_entry_5)
-        self.create_image(120.5, 410.5, image=self.image_entry_5)
-        self.create_image(298.5, 410.5, image=self.image_entry_9)
-        self.create_image(490.5, 410.5, image=self.image_entry_9)
-        self.create_image(629.5, 410.5, image=self.image_entry_9)
-        self.create_image(770.5, 410.5, image=self.image_entry_9)
-
-      
-  
         
+        #PLACE THE IMAGES
+        # self.create_image(405.0, 46.0, image=self.image_image_1)
+        # self.create_image(120.0, 150.5, image=self.image_image_6)
+        # self.create_image(409.0, 190.5, image=self.image_entry_3)
+        # self.create_image(409.0, 237.5, image=self.image_entry_3)
+        # self.create_image(397.0, 120.5, image=self.image_entry_11)
+        # self.create_image(155.5, 120.5, image=self.image_entry_11)
+        # self.create_image(120.5, 310.5, image=self.image_entry_5)
+        # self.create_image(298.5, 310.5, image=self.image_entry_5)   
+        # self.create_image(120.5, 340.5, image=self.image_entry_5)
+        # self.create_image(298.5, 340.5, image=self.image_entry_5)
+        # self.create_image(490.5, 342.5, image=self.image_entry_5)
+        # self.create_image(698.5, 342.5, image=self.image_entry_5)
+        # self.create_image(120.5, 410.5, image=self.image_entry_5)
+        # self.create_image(298.5, 410.5, image=self.image_entry_9)
+        # self.create_image(490.5, 410.5, image=self.image_entry_9)
+        # self.create_image(629.5, 410.5, image=self.image_entry_9)
+        # self.create_image(770.5, 410.5, image=self.image_entry_9)
+
+    
+        self.create_image(120.0, 150.5, image=self.image_image_6)
+        self.create_image(405.0, 46.0, image=self.image_image_1)
+        self.image_ids = [] # Store the image ids to hide them later
+  
+        self.image_ids.append(self.create_image(409.0, 190.5, image=self.image_entry_3))
+        self.image_ids.append(self.create_image(409.0, 237.5, image=self.image_entry_3))
+        self.image_ids.append(self.create_image(397.0, 120.5, image=self.image_entry_11))
+        self.image_ids.append(self.create_image(155.5, 120.5, image=self.image_entry_11))
+        self.image_ids.append(self.create_image(120.5, 310.5, image=self.image_entry_5))
+        self.image_ids.append(self.create_image(298.5, 310.5, image=self.image_entry_5))
+        self.image_ids.append(self.create_image(120.5, 340.5, image=self.image_entry_5))
+        self.image_ids.append(self.create_image(298.5, 340.5, image=self.image_entry_5))
+        self.image_ids.append(self.create_image(490.5, 342.5, image=self.image_entry_5))
+        self.image_ids.append(self.create_image(120.5, 410.5, image=self.image_entry_5))
+        self.image_ids.append(self.create_image(280.5, 410.5, image=self.image_entry_16))
+        self.image_ids.append(self.create_image(472.5, 410.5, image=self.image_entry_16))
+        self.image_ids.append(self.create_image(612.5, 410.5, image=self.image_entry_16))
+        self.image_ids.append(self.create_image(750.5, 410.5, image=self.image_entry_16))      
 
 
+                
+        # Creating and placing the button widget
+        self.button_1 = tk.Button(
+        image=self.button_image_1,
+        borderwidth=0,
+        highlightthickness=0,
+        command = self.hide
+      
+)
+        self.button_1.place(
+        x=405.0,
+        y=450.0,
+        width=100.0,
+        height=30.0
+)
+        self.button_2 = tk.Button(
+        image=self.button_image_1,
+        borderwidth=0,
+        highlightthickness=0,
+        command = self.show
+)   
+        self.button_2.place(
+        x=505.0,
+        y=450.0,
+        width=100.0,
+        height=30.0
+)
+        
     # Creating and placing the entry widget
 
         self.Reference_No = Entry(
@@ -209,6 +251,7 @@ class Register(tk.Canvas):
         width=142.0,
         height=10.0
 )
+
         self.Monthly_Income = Entry(
         bd=0,
         bg="#FFE5AB",
@@ -230,7 +273,7 @@ class Register(tk.Canvas):
         self.Other_Sources_Of_Income.place(
         x=230.0,
         y=405.5,
-        width=110.0,
+        width=105.0,
         height=10.0
 )
         self.Monthly_Expenditure = Entry(
@@ -242,7 +285,7 @@ class Register(tk.Canvas):
         self.Monthly_Expenditure.place(
         x=420.0,
         y=405.5,
-        width=110.0,
+        width=105.0,
         height=10.0
 )
         self.Gross_Monthly_Income = Entry(
@@ -254,7 +297,7 @@ class Register(tk.Canvas):
         self.Gross_Monthly_Income.place(
         x=559.0,
         y=405.5,
-        width=110.0,
+        width=115.0,
         height=10.0
 )
         self.Net_Monthly_Income = Entry(
@@ -266,20 +309,63 @@ class Register(tk.Canvas):
         self.Net_Monthly_Income.place(
         x=700.0,
         y=405.5,
-        width=110.0,
+        width=105.0,
         height=10.0
 )
-     
+
+            
+# HIDE THE WIDGETS
+    def hide(self):
+            for image_id in self.image_ids:
+                self.itemconfigure(image_id, state="hidden")
+            self.Reference_No.place_forget()
+            self.Date.place_forget()
+            self.FullName.place_forget()
+            self.Permanent_Address.place_forget()
+            self.Birthdate.place_forget()
+            self.Age.place_forget()
+            self.Nationality.place_forget()
+            self.Religion.place_forget()
+            self.Occupation.place_forget()
+            self.Monthly_Income.place_forget()
+            self.Other_Sources_Of_Income.place_forget()
+            self.Monthly_Expenditure.place_forget()
+            self.Gross_Monthly_Income.place_forget()
+            self.Net_Monthly_Income.place_forget()
+            self.button_1.place_forget()
+    
+# SHOW THE WIDGETS
+    def show(self):
+        for image_id in self.image_ids:
+            self.itemconfigure(image_id, state="normal")
+            self.Reference_No.place()
+            self.Date.place()
+            self.FullName.place()
+            self.Permanent_Address.place()
+            self.Birthdate.place()
+            self.Age.place()
+            self.Nationality.place()
+            self.Religion.place()
+            self.Occupation.place()
+            self.Monthly_Income.place()
+            self.Other_Sources_Of_Income.place()
+            self.Monthly_Expenditure.place()
+            self.Gross_Monthly_Income.place()
+            self.Net_Monthly_Income.place()
+            self.button_1.place()
+            
+            
+
+
+
+
+
+            
+
         
+       
 
-
-
-
-
-
-
-
-
+    
 
 
 if __name__ == "__main__":
