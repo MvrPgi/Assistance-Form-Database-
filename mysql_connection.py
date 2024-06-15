1db36a41ed23977d175d6ce7bc7d20aa9d33f5de
Mover <movermercene@gmail.com>
2024-06-15 22:55:29 +0800

    Corrected Some Comments

--- mysql_connection.py
+++ mysql_connection.py
@@ -55,15 +55,15 @@ class DatabaseConnection:
   def insert_household_details(self,Hhold_Fam_Name, Hhold_Fam_Age, Hhold_Fam_CivilStatus, Hhold_Fam_RSWithPatient, Hhold_Fam_HighestEducAttain, Hhold_Fam_Occupation, Hhold_Fam_MonthlyIncome):
     household_query = "INSERT INTO household_details (Applicant_ID, Hhold_Fam_Name, Hhold_Fam_Age, Hhold_Fam_CivilStatus, Hhold_Fam_RSWithPatient, Hhold_Fam_HighestEducAttain, Hhold_Fam_Occupation, Hhold_Fam_MonthlyIncome) VALUES (%(Applicant_ID)s, %(Hhold_Fam_Name)s, %(Hhold_Fam_Age)s, %(Hhold_Fam_CivilStatus)s, %(Hhold_Fam_RSWithPatient)s, %(Hhold_Fam_HighestEducAttain)s, %(Hhold_Fam_Occupation)s, %(Hhold_Fam_MonthlyIncome)s)"
     last_applicant_id = self.get_last_applicant_id() # Get the last applicant ID
-    household_data = { # Data to be inserted
-        "Applicant_ID": last_applicant_id,
-        "Hhold_Fam_Name": Hhold_Fam_Name,
-        "Hhold_Fam_Age": Hhold_Fam_Age,
-        "Hhold_Fam_CivilStatus": Hhold_Fam_CivilStatus,
-        "Hhold_Fam_RSWithPatient": Hhold_Fam_RSWithPatient,
-        "Hhold_Fam_HighestEducAttain": Hhold_Fam_HighestEducAttain,
-        "Hhold_Fam_Occupation": Hhold_Fam_Occupation,
-        "Hhold_Fam_MonthlyIncome": Hhold_Fam_MonthlyIncome
+    household_data = {
+      "Applicant_ID": last_applicant_id, # Data to be inserted
+      "Hhold_Fam_Name": Hhold_Fam_Name,
+      "Hhold_Fam_Age": Hhold_Fam_Age,
+      "Hhold_Fam_CivilStatus": Hhold_Fam_CivilStatus,
+      "Hhold_Fam_RSWithPatient": Hhold_Fam_RSWithPatient,
+      "Hhold_Fam_HighestEducAttain": Hhold_Fam_HighestEducAttain,
+      "Hhold_Fam_Occupation": Hhold_Fam_Occupation,
+      "Hhold_Fam_MonthlyIncome": Hhold_Fam_MonthlyIncome
     }
     self.cursor.execute(household_query, household_data)
     self.con.commit()
