1db46c43a0f8cb313058c6564a05ff1a7b6cda59
Mover <movermercene@gmail.com>
2024-06-29 22:52:37 +0800

    Main Window Change

--- main.py
+++ main.py
@@ -10,17 +10,40 @@ class App(tk.Tk):
         super().__init__()
         
         self.title("Main App")
+        self.main_frame = tk.Frame(self, bg="#FFFFFF")
+        self.main_frame.pack(fill=tk.BOTH, expand=True)
+        
         self.geometry("820x500")
         self.resizable(False, False)
-        self.configure(bg="#FFFFFF")
         
+        self.configure(bg="#FFFFFF")
         self.open_admin_bench_button = tk.Button(self, text="Open Admin Bench", command=self.open_admin_bench)
         self.open_admin_bench_button.pack(pady=20)
+        self.open_register_button = tk.Button(self, text="Open Register", command=self.open_register)
+        self.open_register_button.pack(pady=20)
+
+
+
+    def open_register(self):
+        # Clear the main_frame
+        for widget in self.main_frame.winfo_children():
+            widget.destroy()
+        
+        # Create and pack Register canvas into the main_frame
+        register_canvas = Register(master=self.main_frame)
+        register_canvas.pack(fill=tk.BOTH, expand=True)
+        
+
 
     def open_admin_bench(self):
         self.withdraw()
         admin_bench_window = AdminBench()
         admin_bench_window.mainloop()
+
+    def open_register(self):
+        self.withdraw()
+        register_window = Register()
+        register_window.mainloop()
         
 if __name__ == "__main__":
     app = App()
